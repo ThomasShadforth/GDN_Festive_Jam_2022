16cada6312871a540c1972f2596bf9b4ad963962
Tom <u1909810@uel.ac.uk>
2022-12-05 23:45:07 +0000

    Setting up presents for object pooling

---
+++ GDN_Big_Festive_Jam_2022/Assets/Scripts/Visual Scripts/PresentObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentObject : MonoBehaviour
+{
+
+    public Transform handToAppearIn;
+
+    private void OnEnable()
+    {
+        //Apply the velocity depending on the direction faced
+        //Set position to the player/enemy hand (depending on what is set)
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GDN_Big_Festive_Jam_2022/Assets/Scripts/Visual Scripts/PresentObjectPool.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentObjectPool : MonoBehaviour
+{
+    Queue<GameObject> _availableObjects = new Queue<GameObject>();
+
+    public static PresentObjectPool instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void AddToPool(GameObject instanceToAdd)
+    {
+        instanceToAdd.SetActive(false);
+        _availableObjects.Enqueue(instanceToAdd);
+    }
+
+    public GameObject GetFromPool()
+    {
+        var poolInstance = _availableObjects.Dequeue();
+        poolInstance.SetActive(true);
+        return poolInstance;
+    }
+}
